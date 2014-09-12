f7172f74c60979eb4b8801fc749487bc3ece99e2
benburton <benjamin.joseph.burton@gmail.com>
2014-09-12 16:19:57 -0400

    Update jquery path

--- test/test.conf.js
+++ test/test.conf.js
@@ -2,7 +2,7 @@ basePath = '..';
 files = [
   JASMINE,
   JASMINE_ADAPTER,
-  'components/jquery/jquery.js',
+  'components/jquery/dist/jquery.js',
   'components/angular/angular.js',
   'components/angular-mocks/angular-mocks.js',
   'src/directive.js',
